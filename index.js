28592ef6111f265fc5b0676183827948902aeba5
Jurasia <jurasiabritton@gmail.com>
2022-11-18 21:34:24 -0500

    commit tree 3f7486ceba018ae42161683e04e7e3f7c6ecf7f6

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string) {
+    // todo
+  }
+  function shout(string) {
+    return string;
+  }function shout(string) {
+    return string.toUpperCase();
+  }
+  function whisper(string) {
+    //todo
+  }
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+ function logShout (aWord) {
+    console.log(aWord.toUpperCase( ));
+ }
+ function logWhisper (aWord) {
+    console.log(aWord.toLowerCase( ));
+ }
+ function sayHiToHeadphonedRoommate (string) {
+    if (string.toLowerCase()===string) {
+        return "I can't hear you!"
+    }
+ }
